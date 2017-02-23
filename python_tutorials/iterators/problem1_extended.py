0d8cc8b5bb373c96f6dbccbe86f69589aff78bbc
Deepak Labh <labh111349@gmail.com>
2017-02-23 16:57:23 +0530

    two heir iterator

---
+++ python_tutorials/iterators/problem1_extended.py
@@ -0,0 +1,25 @@
+class rev_range:
+    def __init__(self, n):
+	self.n = n
+	#self.i = n 
+    
+    def __iter__(self):
+	return rev_iter(self.n)
+
+
+
+class rev_iter:
+    def __init__(self, n):
+	self.n = n
+	self.i = n
+
+    def __iter__(self):
+	return self
+
+    def next(self):
+	i = self.i
+	if self.i > 0 :
+	    self.i -= 1
+	    return i 
+	else:
+	    raise StopIteration() 
