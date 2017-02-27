baae7c2706e062e30783ab250dc197d8bec9d7f2
Deepak Labh <labh111349@gmail.com>
2017-02-27 18:03:47 +0530

    python adv tutorials

---
+++ python_tutorials/advanced_python/README.md
@@ -0,0 +1,3 @@
+Tutorial from """http://www.python-course.eu/advanced_topics.php"""
+
+Implementation of various applications
---
+++ python_tutorials/advanced_python/state_machine.py
@@ -0,0 +1,11 @@
+'''http://www.python-course.eu/finite_state_machine.php
+Implement state machine
+'''
+
+class state_machine:
+    def __init__(self):
+	self.handlers = {}
+	self.start_state = None
+	self.end_state = []
+
+    def
