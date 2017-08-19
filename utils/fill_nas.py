8ce1162d7a6a48a98384199c45f48cc6c1ab72d4
DeepakLabh <labh111349@gmail.com>
2017-08-19 23:26:18 +0530

    update

--- utils/fill_nas.py
+++ utils/fill_nas.py
@@ -2,4 +2,9 @@ import pandas as pd
 import numpy as np
 
 class Fillna:
-    def 
+    def __init__(self, file_path, key=''):
+        self.file_path = file_path
+        self.key = key
+
+    def find_correlated_columns(self, key):
+        pass
